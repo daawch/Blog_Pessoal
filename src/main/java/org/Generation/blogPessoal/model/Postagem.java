2e9ea6e3dd682b45b0a0cade02b2052a4895d70d
daawch <caique.rdg.0@gmail.com>
2022-02-21 22:43:53 -0300

    @daawch[TASK:Boas práticas de construção de uma API Rest]

--- src/main/java/org/Generation/blogPessoal/model/Postagem.java
+++ src/main/java/org/Generation/blogPessoal/model/Postagem.java
@@ -7,11 +7,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 
 @Entity
@@ -32,7 +35,11 @@ public class Postagem {
 	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date data = new java.sql.Date(System.currentTimeMillis());
-
+	
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
+	
 	public long getId() {
 		return id;
 	}
@@ -65,6 +72,20 @@ public class Postagem {
 		this.data = date;
 	}
 	
-	
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
+
+	public Date getData() {
+		return data;
+	}
+
+	public void setData(Date data) {
+		this.data = data;
+	}
 	
 }
