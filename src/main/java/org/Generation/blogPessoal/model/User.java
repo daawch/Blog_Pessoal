ab2f5a8b11a6a754e9149729203590ea5efe9435
daawch <caique.rdg.0@gmail.com>
2022-03-15 22:32:54 -0300

    @daawch[adding branch test]

--- src/main/java/org/Generation/blogPessoal/model/User.java
+++ src/main/java/org/Generation/blogPessoal/model/User.java
@@ -5,8 +5,11 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import io.swagger.v3.oas.annotations.media.Schema;
 
 @Entity
 @Table(name = "tb_usuario")
@@ -15,12 +18,14 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
 	@NotNull
 	@Size(min = 2, max = 100)
 	private String nome;
 
-	@NotNull
-	@Size(min = 5, max = 100)
+	@Schema(example = "email@email.com.br")
+	@NotNull(message = "O atributo Usuário é Obrigatório!")
+	@Email(message = "O atributo Usuário deve ser um email válido!")
 	private String usuario;
 	
 	@Size(min = 0, max = 5000)
